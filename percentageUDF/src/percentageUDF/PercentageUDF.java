a4ceeac94284c8505f789ae46a76815fe34ff89c
Sai kishore Amirishetty <sai.kishi2010@gmail.com>
2017-02-07 00:05:42 +0530

    Update PercentageUDF.java

--- percentageUDF/src/percentageUDF/PercentageUDF.java
+++ percentageUDF/src/percentageUDF/PercentageUDF.java
@@ -4,9 +4,10 @@ import java.io.IOException;
 import org.apache.pig.FilterFunc;
 import org.apache.pig.data.Tuple;
 public class PercentageUDF extends FilterFunc {
+	//passing the objective and performance and reading them as tuple
 	public Boolean exec(Tuple input) throws IOException {
 		try {
-			//String District = (String)input.get(0);
+			
 			int objective= (int)input.get(1);
 			int performance= (int)input.get(2);
 			
