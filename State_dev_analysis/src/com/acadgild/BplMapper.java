9a68dd148dc298cfaa998a649278bcf31f489b4c
Sai kishore Amirishetty <sai.kishi2010@gmail.com>
2017-02-07 00:09:26 +0530

    Update BplMapper.java

--- State_dev_analysis/src/com/acadgild/BplMapper.java
+++ State_dev_analysis/src/com/acadgild/BplMapper.java
@@ -14,14 +14,14 @@ import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
  
 public class BplMapper extends Mapper<LongWritable, Text, Text, TextPair> {
- 
+ //Declaring the textpair to add the objective and performance.
    private static TextPair BPL_data = new TextPair();
    
  
     public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
  
         try {
- 
+           //  To read the data from XML and adding them as document
             InputStream is = new ByteArrayInputStream(value.toString().getBytes());
             DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
             DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
@@ -30,21 +30,23 @@ public class BplMapper extends Mapper<LongWritable, Text, Text, TextPair> {
             doc.getDocumentElement().normalize();
             //to get aal the nodes under the tag name
             NodeList nList = doc.getElementsByTagName("row");
- 
+            //iterating through the list of nodes.
             for (int temp = 0; temp < nList.getLength(); temp++) {
- 
+          
                 Node nNode = nList.item(temp);
  
                 if (nNode.getNodeType() == Node.ELEMENT_NODE) {
  
                     Element eElement = (Element) nNode;
- 
+                   //getting State,project_objective,project_performance from the nodes
                     String State= eElement.getElementsByTagName("State_Name").item(0).getTextContent();
                     String Project_objective_BPL = eElement.getElementsByTagName("Project_Objectives_IHHL_BPL").item(0).getTextContent();
                     String Project_Performance_BPL = eElement.getElementsByTagName("Project_Performance-IHHL_BPL").item(0).getTextContent();
                     Text objective_BPL=new Text(Project_objective_BPL);
                     Text performance_BPL=new Text(Project_Performance_BPL);
+                 //Adding the project_objective,project_performance to the TextPair
                     BPL_data.set(objective_BPL, performance_BPL);
+                 //Writing the State and textPair as a map output
                     context.write(new Text(State),BPL_data );
  
                 }
