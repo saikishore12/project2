a140b4cadcc4fdf5f2581cb67bd9a5c4f8ceae7e
Sai kishore Amirishetty <sai.kishi2010@gmail.com>
2017-03-08 11:30:45 +0530

    Update PercentageUDF.java

--- percentageUDF/src/percentageUDF/PercentageUDF.java
+++ percentageUDF/src/percentageUDF/PercentageUDF.java
@@ -7,17 +7,17 @@ public class PercentageUDF extends FilterFunc {
 	//passing the objective and performance and reading them as tuple
 	public Boolean exec(Tuple input) throws IOException {
 		try {
-			
+			 String District=(String)Input.get(0);
 			int objective= (int)input.get(1);
 			int performance= (int)input.get(2);
 			
 			int temp=(objective-performance)/objective;
 			int percentage=temp*100;
 			if (percentage==80) {
-				return true;
+				return District;
 			}
 			else {
-				return false;
+				return null;
 			}
 		}
 		catch (Exception e) {
